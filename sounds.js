998fc9be2fffeea41947d8b66d80593bf1d5d879
asell22 <alexanderjsell@gmail.com>
2015-01-20 14:15:05 -0800

    Different sounds for different buttons

--- sounds.js
+++ sounds.js
@@ -1,7 +1,9 @@
+// define variable moveButton to function that causes button to have button active class.
 var moveButton = function(button){
   button.className += " active";
 }
 
+//declare variable addButton to anonymous function. In function, declare variable btn to inserting a "div" element to the document. Declare variable buttonNumber to randomizing numbers from range 0 to 3 (0,1,2,3). The class name of the button  is declared using the buttonNumber variable. Declare variable board to be the first element in the board array. Add a button to variable board. Returns button.
 var addButton = function(){
   var btn = document.createElement("div")
 
@@ -12,7 +14,8 @@ var addButton = function(){
   return btn;
 }
 
-generateButtons = function(){
+//Declare generateButtons to anonymous function. Recursively calls it self. Function called after 1.5 seconds. Declare variable x to calling addButton function.
+var generateButtons = function(){
 
   setTimeout(function(){
     generateButtons();
@@ -24,7 +27,7 @@ generateButtons = function(){
   setTimeout(function(){
     moveButton(x);
     setTimeout(function(){
-     console.log(x.className + " button crossed")
+     soundButtons(x); // instead of logging what specific button crosses, we need the individual sounds to sound
     }, 2769)
 
   }, 10)
@@ -35,7 +38,35 @@ setTimeout(function(){
 
 var t = 2000;
 
-var theme = new Howl({
-  urls: ['/audio/musical_electric_guitar_distorted_c_sus2.mp3'],
-  loop: true
-}).play();
+var theme1 = new Howl({
+  urls: ['/audio/electric_bass_scrape_down.mp3'],
+  loop: false
+})
+
+var theme2 = new Howl({
+  urls: ['/audio/electric_bass_scrape_up.mp3'],
+  loop: false
+})
+
+var theme3 = new Howl({
+  urls: ['/audio/musical_organ_blues_riff.mp3'],
+  loop: false
+})
+
+var theme4 = new Howl({
+  urls: ['/audio/musical_violin_trill_short.mp3'],
+  loop: false
+})
+
+var soundButtons = function(button){
+
+  if (button.className === "button button0 active") {
+    theme1.play();
+  } else if(button.className === "button button1 active") {
+    theme2.play();
+  } else if(button.className === "button button2 active") {
+    theme3.play();
+  } else {
+    theme4.play();
+  }
+}
